c90682703eb77727f8c5974215d418ad8afcb7f7
madhulikaa223344 <madhulikaa223344@gmail.com>
2024-04-18 17:15:46 +0530

    added cors

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const cors=require('cors');
+const fs = require("fs");
 
 const app = express();
 
 const PORT = 8000;
 const data = require('./data/questions.json');
+app.use(cors());
+app.options("*", cors());
 
 app.get('/', (req, res) => {
-  res.json(data);
+    fs.readFile('./data/questions.json',"utf-8", (err, data) => {
+      if(err)console.log(err)
+      const result = JSON.parse(data)
+      res.send(result);
+    })
 })
 
 app.listen(PORT, () =>{
